fb9fe08fd2285804d390e54d06a28a1677fad690
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-20 14:27:09 +0800

    build: create reference

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,11 @@
 const selectField = document.querySelector("#select-field");
 const selectText = document.querySelector("#select-text");
 const options = document.querySelectorAll(".options");
+const list = document.querySelector(".list");
 
 options.forEach((option) => {
   option.addEventListener("click", () => {
     selectText.textContent = option.textContent;
   })
 })
+
