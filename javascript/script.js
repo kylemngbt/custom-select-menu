bdc0c7881891837179f055cb961ab4d6d9e9bab2
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-20 14:25:23 +0800

    feat: add click event to all the options

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,9 @@
 const selectField = document.querySelector("#select-field");
 const selectText = document.querySelector("#select-text");
-const options = document.querySelector(".options");
+const options = document.querySelectorAll(".options");
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    selectText.textContent = option.textContent;
+  })
+})
